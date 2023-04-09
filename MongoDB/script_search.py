443639b5ffb70accd77bc21c694dcfd4f5b5db86
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-04-09 18:03:31 +0300

    final version
    with additional task

--- MongoDB/script_search.py
+++ MongoDB/script_search.py
@@ -4,38 +4,34 @@ from MongoDB.models import Quotes, Authors
 import connect_bd
 from redis_lru import RedisLRU
 
-client = redis.StrictRedis(host='localhost', port=6339, password=1234567890, username=nata)
+client = redis.StrictRedis(host='localhost', port=6379, password=None)
 cache = RedisLRU(client)
 
-@cache
+
 def name_command(text_command):
     try:
         id_name = Authors.objects(fullname=text_command)
         for id_n in id_name:
-            # print(id_n.id)
             result = Quotes.objects(author=id_n)
-            for q in result:
-                print(str(q.id).encode(encoding='UTF-8'), str(q.tags).encode(encoding='UTF-8'),
-                      str(q.quote).encode(encoding='UTF-8'))
+            return result
+
     except:
         print("Not found!")
-@cache
+
 def tag_command(text_command):
     try:
         result = Quotes.objects(tags=text_command)
-        for q in result:
-            print(str(q.id).encode(encoding='UTF-8'), str(q.tags).encode(encoding='UTF-8'),
-                  str(q.quote).encode(encoding='UTF-8'))
+        return result
+
     except:
         print("Not found!")
-@cache
+
+
 def tags_command(text_command):
     try:
         list_tags = text_command.split(",")
         result = Quotes.objects(tags__in=list_tags)
-        for q in result:
-            print(str(q.id).encode(encoding='UTF-8'), str(q.tags).encode(encoding='UTF-8'),
-                  str(q.quote).encode(encoding='UTF-8'))
+        return result
     except:
         print("Not found!")
 
@@ -45,11 +41,32 @@ def main():
         if command == 'exit':
             break
         list_command = command.split(":")
+        for key in client.scan_iter():
+            if command.startswith(key.decode()):
+                cache.get(key)
+                print("This is result in cache!")
+                continue
         match list_command[0]:
             case 'exit': break
-            case 'name': name_command(list_command[1])
-            case 'tag': tag_command(list_command[1])
-            case 'tags': tags_command(list_command[1])
+            case 'name':
+                result = name_command(list_command[1])
+                cache.set(command, result)
+                for q in result:
+                    print(str(q.id).encode(encoding='UTF-8'), str(q.tags).encode(encoding='UTF-8'),
+                          str(q.quote).encode(encoding='UTF-8'))
+            case 'tag':
+                result = tag_command(list_command[1])
+                cache.set(command, result)
+                for q in result:
+                    print(str(q.id).encode(encoding='UTF-8'), str(q.tags).encode(encoding='UTF-8'),
+                          str(q.quote).encode(encoding='UTF-8'))
+            case 'tags':
+                result = tags_command(list_command[1])
+                cache.set(command, result)
+                for q in result:
+                    print(str(q.id).encode(encoding='UTF-8'), str(q.tags).encode(encoding='UTF-8'),
+                          str(q.quote).encode(encoding='UTF-8'))
+
 
 
 if __name__ == "__main__":
